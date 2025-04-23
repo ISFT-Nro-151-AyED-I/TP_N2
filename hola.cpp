32a7f90b85c7c575cc772fad1aac815f208834a9
davidhernanbravo <bdavidhernan@gmail.com>
2025-04-23 00:42:59 -0300

    Create hola.cpp

---
+++ hola.cpp
@@ -0,0 +1,12 @@
+/* TP N°2 - MARCO PRÁCTICO
+
+    1. Crear el Primer Programa “Hola Mundo!”. */
+
+#include <iostream>
+using namespace std;
+
+int main()
+    {
+        cout << "Hola Mundo!" << endl;
+        return 0;
+    }
